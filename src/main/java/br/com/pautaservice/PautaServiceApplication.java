829434ddba93878b4fc37fda97ca9f660644703a
Diovane <diovane.m.mendes@gmail.com>
2020-06-19 16:01:22 -0300

    [ESTRUTURA] #1
    Estruturação do projeto

---
+++ src/main/java/br/com/pautaservice/PautaServiceApplication.java
@@ -0,0 +1,11 @@
+package br.com.pautaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PautaServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PautaServiceApplication.class, args);
+	}
+}
